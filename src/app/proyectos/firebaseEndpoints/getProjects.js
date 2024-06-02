aefca12f0523e118ea03a69bc90aadcad0fc83d1
neleon10 <cvn45@hotmail.com>
2024-06-02 16:20:42 +0200

    modified to return a promise

--- src/app/proyectos/firebaseEndpoints/getProjects.js
+++ src/app/proyectos/firebaseEndpoints/getProjects.js
@@ -2,12 +2,18 @@ import { query, ref, onValue, orderByKey } from 'firebase/database';
 import { db } from '@/Firebase/credential';
 
 export function getProjects() {
-  var withdrawRef = query(ref(db, `/projects/`), orderByKey())
-  onValue(withdrawRef, snapshot => {
-    const data = snapshot.val()
-    if (data !== null) {
-      const projects = Object.values(data);
-      return projects;
-    }
+  return new Promise((resolve, reject) => {
+    const withdrawRef = query(ref(db, `/projects/`), orderByKey());
+    onValue(withdrawRef, snapshot => {
+      const data = snapshot.val();
+      if (data !== null) {
+        const projects = Object.values(data);
+        resolve(projects);
+      } else {
+        reject(new Error("No projects found"));
+      }
+    }, error => {
+      reject(error);
+    });
   });
 }
