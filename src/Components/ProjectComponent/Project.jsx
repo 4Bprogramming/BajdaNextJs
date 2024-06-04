0ae352e36482b8c637660a05d40e2291962667d5
neleon10 <cvn45@hotmail.com>
2024-06-04 22:55:58 +0200

    useeffect added

--- src/Components/ProjectComponent/Project.jsx
+++ src/Components/ProjectComponent/Project.jsx
@@ -1,24 +1,21 @@
 
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { getProjectById } from '@/app/proyectos/firebaseEndpoints/getProjectId'
 import { usePathname } from 'next/navigation'
-import { suspend } from "suspend-react";
-
-
-async function getProject() {
-  try {
-    const projectId = usePathname().split("/").at(2);
-    const project = getProjectById(projectId);
-    return project;
-  } catch (error) {
-    console.error('Error fetching projects:', error);
-    throw error;
-  }
-}
 
 function Project() {
-    let project = suspend(getProject);
+  const projectId = usePathname().split("/").at(2);
+  const [project, setProject] = useState({});
+  useEffect(()=>{
+    async function getProject (){
+      const response = await getProjectById(projectId);
+      setProject(response)
+    } 
+    getProject();
+    
+  },[]);
+ 
   return (
     <div>{project.title}</div>
   )
