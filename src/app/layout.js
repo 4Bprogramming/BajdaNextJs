96b3e8d903c563a2d36ba02a5211ef13c667d394
neleon10 <cvn45@hotmail.com>
2024-06-05 21:54:11 +0200

    script added before the body closing tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        {children}
+        <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+      </body>
     </html>
   );
 }
