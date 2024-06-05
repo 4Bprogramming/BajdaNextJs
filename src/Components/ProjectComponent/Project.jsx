fd3049db153521dc4b71756ac7cc8a241aa633fc
neleon10 <cvn45@hotmail.com>
2024-06-05 21:54:33 +0200

    project in process

--- src/Components/ProjectComponent/Project.jsx
+++ src/Components/ProjectComponent/Project.jsx
@@ -1,24 +1,35 @@
-
-'use client'
-import React, { useEffect, useState } from 'react'
-import { getProjectById } from '@/app/proyectos/firebaseEndpoints/getProjectId'
-import { usePathname } from 'next/navigation'
+"use client";
+import React, { useEffect, useState } from "react";
+import { getProjectById } from "@/app/proyectos/firebaseEndpoints/getProjectId";
+import { usePathname } from "next/navigation";
 
 function Project() {
   const projectId = usePathname().split("/").at(2);
   const [project, setProject] = useState({});
-  useEffect(()=>{
-    async function getProject (){
+  useEffect(() => {
+    async function getProject() {
       const response = await getProjectById(projectId);
-      setProject(response)
-    } 
+      setProject(response);
+    }
     getProject();
-    
-  },[]);
- 
+  }, []);
+
   return (
-    <div>{project.title}</div>
-  )
+    <section>
+      <div className="w-full p-8 border-l-[3px] border-l-custom-green ml-4 mt-4">
+        <h1 className="text-2xl">{project.title}</h1>
+        <p className=" capitalize">{project.place}</p>
+      </div>
+      
+
+
+
+
+
+
+
+    </section>
+  );
 }
 
-export default Project
+export default Project;
