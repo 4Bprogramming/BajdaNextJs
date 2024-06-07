3db3269727ece1973f7709f727a74d5149e9ef46
neleon10 <cvn45@hotmail.com>
2024-06-07 15:53:11 +0200

    get projects pages moved

---
+++ prisma/migrations/20240607133324_firstmigration/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Project` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `place` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
