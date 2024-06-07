cacb60af6495fc83e3a114fada55c862944eaad0
neleon10 <cvn45@hotmail.com>
2024-06-07 16:32:02 +0200

    api call by ID

--- src/app/api/projects/route.js
+++ src/app/api/projects/route.js
@@ -2,15 +2,32 @@ const { PrismaClient } = require('@prisma/client')
 import { NextResponse } from 'next/server'
 const prisma = new PrismaClient()
 
-export async function GET() {
-    try {
+export async function GET(request) {
+  try {
+    const { id } = request.params;
+
+    if (id) {
+      const project = await prisma.project.findUnique({
+        where: {
+          id: parseInt(id),
+        },
+      });
+
+      if (!project) {
+        return NextResponse.json({ error: 'Project not found' }, { status: 404 });
+      }
+
+      return NextResponse.json(project, { status: 200 });
+    } else {
       const projects = await prisma.project.findMany();
       return NextResponse.json(projects, { status: 200 });
-    } catch (error) {
-      console.error(error);
-      return NextResponse.json({ error: 'Failed to fetch projects' }, { status: 500 });
-    } 
+    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'Failed to fetch projects' }, { status: 500 });
   }
+}
+
 
   export async function POST(request) {
     try {
