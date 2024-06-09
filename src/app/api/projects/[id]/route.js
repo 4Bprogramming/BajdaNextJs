cc08dab3dc3a75f4162b35a16a8aeb79f6981c4d
gabdala17 <gisella.abdala@gmail.com>
2024-06-09 13:21:48 -0300

    images included

--- src/app/api/projects/[id]/route.js
+++ src/app/api/projects/[id]/route.js
@@ -10,6 +10,7 @@ export async function GET(request, {params}){
             where: {
               id: id
             },
+            include: { images: true }
           })
 
         if (!project) { return NextResponse ('no esta el proyecto', {status:404})
@@ -19,4 +20,21 @@ export async function GET(request, {params}){
     } catch (error) {
         console.log('error de traer por id===>', error)
     }
+}
+export default async function handler(req, res) {
+  const { id } = req.query;
+
+  if (req.method === 'PUT') {
+    try {
+      const updatedProject = await prisma.project.update({
+        where: { id: parseInt(id) },
+        data: req.body,
+      });
+      res.status(200).json(updatedProject);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to update project' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
 }
