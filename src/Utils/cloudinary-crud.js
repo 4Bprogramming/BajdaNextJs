66a434fd43236322a2a038e89699ff30e1fd875e
neleon10 <cvn45@hotmail.com>
2024-06-11 01:41:41 +0200

    cloudinary url array replaced by object

--- src/Utils/cloudinary-crud.js
+++ src/Utils/cloudinary-crud.js
@@ -1,48 +1,41 @@
-
 import { CLOUDINARY } from "@/constants/constants";
 
-export async function getImageUrls(file, files = []) {
+export async function getImageCloudinaryObject(file, files = []) {
   const formData = new FormData();
-  
-  // Verifica si 'file' está presente y agrégalo al FormData
-  
   if (file) {
     formData.append("image", file);
   }
 
-  // Verifica si 'files' no está vacío y agrégalo al FormData
   if (files.length > 0) {
     for (let i = 0; i < files.length; i++) {
       formData.append("imagesArray", files[i]);
     }
   }
-  console.log(formData);
 
-  // Mando los archivos de imagen al backend para que me devuelva URLs
   const response = await fetch(CLOUDINARY, {
     method: "POST",
     body: formData
   });
 
   if (response.ok) {
-    const urls = await response.json();
-    return urls;
+    const cloudinaryObjectArray = await response.json();
+    return cloudinaryObjectArray;
   } else {
     console.log("Oops! ", response.statusText);
   }
 }
 export async function deleteImage(imageId, projectId) {
-  console.log('imageId==>', imageId);
+  console.log("imageId==>", imageId);
   const response = await fetch(CLOUDINARY, {
-    method: 'DELETE',
-    
-    body: JSON.stringify({ imageId, projectId }),
+    method: "DELETE",
+
+    body: JSON.stringify({ imageId, projectId })
   });
 
   if (response.ok) {
-    console.log('Image deleted successfully');
+    console.log("Image deleted successfully");
     // Actualiza tu estado o realiza cualquier otra acción necesaria
   } else {
-    console.error('Failed to delete image');
+    console.error("Failed to delete image");
   }
 }
