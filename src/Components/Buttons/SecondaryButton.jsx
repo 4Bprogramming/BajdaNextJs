03632d509762a26b3d95b56f93dfbd406fa2cfed
neleon10 <cvn45@hotmail.com>
2024-06-12 11:38:06 +0200

    styles modified

--- src/Components/Buttons/SecondaryButton.jsx
+++ src/Components/Buttons/SecondaryButton.jsx
@@ -5,7 +5,7 @@ function SecondaryButton(props) {
   return (
     <Link href={props.href} passHref legacyBehavior >
     <button
-      class={`bg-slate-500 cursor-pointer shadow-buttonShadow px-2 text-xl text-white rounded-2xl w-fit relative after:content-['»'] after:absolute after:opacity-0 after:right-2 hover:after:transition-all duration-700 hover:after:opacity-100 hover:after:right-4 hover:pr-8 ${props.style}`}
+      class={`bg-transparent outline outline-2 outline-custom-green cursor-pointer shadow-buttonShadow px-2 text-xl text-custom-green rounded-2xl w-fit relative after:content-['»'] after:absolute after:opacity-0 after:right-2 hover:after:transition-all duration-700 hover:after:opacity-100 hover:after:right-4 hover:pr-8 hover:text-white hover:bg-custom-green ${props.style}`}
     >
       {props.text}
     </button>
