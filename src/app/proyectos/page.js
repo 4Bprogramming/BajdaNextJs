37867b3ac4b31e5b6c29ebe13e351b6a47775b8b
neleon10 <cvn45@hotmail.com>
2024-06-12 10:33:11 +0200

    seconday button created

--- src/app/proyectos/page.js
+++ src/app/proyectos/page.js
@@ -2,14 +2,12 @@
 import React, {Suspense } from "react";
 
 import Card from "@/Components/ProjectGallery/Card";
-import GreenButton from "@/Components/Buttons/GreenButton";
-import Loading from "./loading";
-import CardSkeleton from "@/Components/Skeletons/CardSkeleton";
+import SecondaryButton from "@/Components/Buttons/SecondaryButton";
 
 function page() {
   return (
     <section className="mb-3">
-      <GreenButton href="/" text="Atrás" style="my-3 ml-1 lg:ml-60" />
+      <SecondaryButton href="/" text="Atrás" style="my-3 ml-1 lg:ml-60" />
       <article className=" my-4 ml-1 max-w-4xl m-auto border-l-[3px] border-custom-green px-3 py-4 sm:ml-5 lg:m-auto lg:max-w-5xl lg:my-2">
         <h1 className="text-4xl font-bold">Proyectos</h1>
         <p className=" text-justify pt-3">
