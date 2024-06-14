d6115803205b57cb6da6134f1bf1241c86ad686d
neleon10 <cvn45@hotmail.com>
2024-06-14 12:44:23 +0200

    component modifed

--- src/app/admin/edit/[id]/page.js
+++ src/app/admin/edit/[id]/page.js
@@ -12,10 +12,9 @@ const EditProjectPage = () => {
   
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Edit Project</h1>
+    
       <EditProjectForm  />
-    </div>
+ 
   );
 };
 
