3fc15b2812b6f94147b95e1212333c584742af6b
neleon10 <cvn45@hotmail.com>
2024-06-15 20:52:40 +0200

    grey colour added to body

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es" className="scroll-smooth">
+    <html lang="es" className="scroll-smooth bg-grey-body">
       <body className={poppins.className}>
         {children}
       </body>
