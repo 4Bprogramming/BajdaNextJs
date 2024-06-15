d517ca51753e1c35e180788189ff98df9f91c8a4
neleon10 <cvn45@hotmail.com>
2024-06-16 00:21:15 +0200

    IG logo added

--- src/Components/Contact/ContactMedia.jsx
+++ src/Components/Contact/ContactMedia.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'next/link'
+import { FaInstagram } from "react-icons/fa";
 
 function ContactMedia() {
   return (
@@ -13,6 +14,10 @@ function ContactMedia() {
         <h4 class=" text-2xl font-semibold text-grey-1">Encuéntranos</h4>
         <p>Moreno 750. 3º "313"</p>
         <p>Bariloche, Rio Negro, Argentina.</p>
+        <Link className='relative underline underline-offset-2 hover:text-custom-green transition-all duration-700 group hover:ml-2' href={"https://www.instagram.com/arq.martin_bajda/?igsh=YmhqdHNqdmxlZGJs"} target='blank'>
+        Visitá Nuestro Instagram: 
+        <FaInstagram className="text-green-500 h-6 w-6 cursor-pointer absolute top-0 -right-8 group-hover:text-black   transition-all duration-700" />
+        </Link>
         
       </find-us>
     </media-container>
