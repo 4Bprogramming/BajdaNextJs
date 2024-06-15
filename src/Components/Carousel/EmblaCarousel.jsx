ea65ce2eff6dea82f93365e4c3d98a26ee88670e
neleon10 <cvn45@hotmail.com>
2024-06-15 22:54:39 +0200

    carousel new feature added

--- src/Components/Carousel/EmblaCarousel.jsx
+++ src/Components/Carousel/EmblaCarousel.jsx
@@ -1,33 +1,54 @@
-import React from 'react'
-import { DotButton, useDotButton } from './EmblaCarouselDotButton'
+import React, { useState } from "react";
+import { DotButton, useDotButton } from "./EmblaCarouselDotButton";
 import {
   PrevButton,
   NextButton,
   usePrevNextButtons
-} from './EmblaCarouselArrowButtons'
-import useEmblaCarousel from 'embla-carousel-react'
-import Image from 'next/image'
+} from "./EmblaCarouselArrowButtons";
+import useEmblaCarousel from "embla-carousel-react";
+import Image from "next/image";
 
 const EmblaCarousel = (props) => {
-  const { slides, options } = props
-  const [emblaRef, emblaApi] = useEmblaCarousel(options)
+  const { slides, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
   const { selectedIndex, scrollSnaps, onDotButtonClick } =
-    useDotButton(emblaApi)
-
+    useDotButton(emblaApi);
+  const [enlargedImageSrc, setEnlargedImageSrc] = useState("");
   const {
     prevBtnDisabled,
     nextBtnDisabled,
     onPrevButtonClick,
     onNextButtonClick
-  } = usePrevNextButtons(emblaApi)
- 
+  } = usePrevNextButtons(emblaApi);
+
+  function handleEnlargeImage(url) {
+    setEnlargedImageSrc(url);
+  }
+
   return (
     <section className="embla">
       <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
-          {slides?.map((slide,index) => (
+          {slides?.map((slide, index) => (
             <div className="embla__slide" key={index}>
-              <Image src={slide.url} alt='Imagen de la casa' fill/>
+              {window.innerWidth >= 1024 ? (
+                <Image
+                  src={slide.url}
+                  alt={`Imagen de la casa ${index}`}
+                  layout="fill"
+                  className="embla__slide__img"
+                  onClick={() => handleEnlargeImage(slide.url)}
+                  sizes=""
+                />
+              ) : (
+                <Image
+                  src={slide.url}
+                  alt={`Imagen de la casa ${index}`}
+                  layout="fill"
+                  className="embla__slide__img"
+                  sizes=""
+                />
+              )}
             </div>
           ))}
         </div>
@@ -44,15 +65,27 @@ const EmblaCarousel = (props) => {
             <DotButton
               key={index}
               onClick={() => onDotButtonClick(index)}
-              className={'embla__dot'.concat(
-                index === selectedIndex ? ' embla__dot--selected' : ''
+              className={"embla__dot".concat(
+                index === selectedIndex ? " embla__dot--selected" : ""
               )}
             />
           ))}
         </div>
       </div>
-    </section>
-  )
-}
 
-export default EmblaCarousel
+      {/* Enlarged Image */}
+      {enlargedImageSrc && <div className="overlay"></div>}
+      <div
+        id="enlargedImageContainer"
+        className={enlargedImageSrc ? "show" : ""}
+        onClick={() => setEnlargedImageSrc("")}
+      >
+        {enlargedImageSrc && (
+          <Image src={enlargedImageSrc} alt="Imagen agrandada" fill  />
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default EmblaCarousel;
