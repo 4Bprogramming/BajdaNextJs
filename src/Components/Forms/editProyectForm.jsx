c259d3bef810ceb2717933eb7d62b3bc1d60a1f9
gabdala17 <gisella.abdala@gmail.com>
2024-06-15 00:34:09 -0300

    remove edit's image fields

--- src/Components/Forms/editProyectForm.jsx
+++ src/Components/Forms/editProyectForm.jsx
@@ -1,12 +1,10 @@
 import { useEffect, useState } from "react";
-import { FaTrash, FaPlus } from "react-icons/fa";
 import {
-  deleteImageCloudinary,
-  getImageCloudinaryObject
-} from "@/Utils/cloudinary-crud";
+  deleteImageCloudinary} from "@/Utils/cloudinary-crud";
 import { usePathname } from "next/navigation";
 import { getProjectById, updateProject } from "@/Utils/project-crud";
 import SecondaryButton from "../Buttons/SecondaryButton";
+import FormImages from "./formImages";
 
 const EditProjectForm = () => {
   const projectId = usePathname().split("/").at(3);
@@ -61,39 +59,7 @@ const EditProjectForm = () => {
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleImageChange = async (e) => {
-    const imageURLs = await getImageCloudinaryObject(e.target.files[0]);
-    setImageFile(imageURLs);
-  };
-
-  const handleImagesChange = async (e) => {
-    const files = Array.from(e.target.files);
-    const newImages = await getImageCloudinaryObject(0, files);
-    setImages([...images, ...newImages]);
-  };
-
-  const handleRemoveImage = (index) => {
-    const deleteToImage = images.filter((_, i) => i === index)[0];
-    const otherImages = images.filter((_, i) => i === index).flat();
-    if (deleteToImage.main === false) {
-      setDeleteImageDB([...deleteImageDB, deleteToImage]);
-      return setImages(images.filter((_, i) => i !== index));
-    } else {
-      deleteImageCloudinary(deleteToImage.public_id);
-      setImages(otherImages);
-    }
-  };
-
-  const handleRemoveMainImage = () => {
-    if (imageFile.cloudinaryID) {
-      setDeleteImageDB([...deleteImageDB, imageFile]);
-      setImageFile("");
-    } else {
-      deleteImageCloudinary(imageFile[0].public_id);
-      setImageFile("");
-    }
-  };
-
+ 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const id = project.id;
@@ -126,7 +92,9 @@ const EditProjectForm = () => {
       if (response) {
         setProjectEdited(true);
         setLoader(false);
-        setFormData(formDataInitialValue);
+        setFormData(initialFormState);
+        setImageFile({})
+        setImages([])
       } else {
         setLoader(false);
         setProjectNotEdited(true);
@@ -190,83 +158,9 @@ const EditProjectForm = () => {
           className="w-full border rounded  focus:outline-custom-green py-1 pl-5 h-12"
           />
       </div>
-      <div>
-        <label className="block text-xl font-medium text-custom-green my-2">
-          Imagen Principal
-        </label>
-        <div className=" flex ">
-          {imageFile ? (
-            <div className="relative">
-              <img
-                src={
-                  typeof imageFile === "object" && imageFile.main
-                  ? imageFile.url
-                  : (Array.isArray(imageFile) &&
-                  imageFile[0] &&
-                  imageFile[0].secure_url) ||
-                  null
-                }
-                alt="Project Image"
-                className="object-cover h-36 w-full md:h-52"
-                />
-              <button
-                type="button"
-                onClick={handleRemoveMainImage}
-                className="absolute top-1 right-1 bg-red-500 text-white rounded-full p-1"
-                >
-                <FaTrash />
-              </button>
-            </div>
-          ) : (
-            <input
-            type="file"
-            accept="image/*"
-            name="image"
-            placeholder="Portada"
-            onChange={handleImageChange}
-            />
-          )}
-        </div>
-      </div>
-      <div>
-        <label className="block text-xl font-medium text-custom-green mt-8 mb-2">
-          Imagenes 
-        </label>
-        <div className="mt-2 flex items-center">
-          <label className="cursor-pointer">
-            <FaPlus className="text-blue-500 mb-2 hover:text-custom-green" size={30} />
-            <input
-              type="file"
-              multiple
-              name="images"
-              onChange={handleImagesChange}
-              className="hidden"
-              />
-          </label>
-        </div>
-        <div className=" grid grid-cols-2 gap-1 row md:grid-cols-3">
-          {images.map((image, index) => (
-            <div key={index} className="relative">
-              <img
-                src={
-                  typeof image === "object" && image.secure_url
-                  ? image.secure_url
-                  : image.url
-                }
-                alt={`Imagen del proyecto`}
-                className="object-cover h-36 w-full md:h-48"
-                />
-              <button
-                type="button"
-                onClick={() => handleRemoveImage(index)}
-                className="absolute top-1 right-1 bg-red-500 text-white rounded-full p-1 cursor-pointer"
-                >
-                <FaTrash />
-              </button>
-            </div>
-          ))}
-        </div>
-      </div>
+     
+        <FormImages images={images} imageFile={imageFile} setImages={setImages} setImageFile={setImageFile} setDeleteImageDB={setDeleteImageDB} deleteImageDB={deleteImageDB} />
+   
       <div>
         <label className="block text-xl font-medium text-custom-green mt-4">Cant. de Habitaciones</label>
         <input
