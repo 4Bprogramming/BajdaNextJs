8f1cb82da486f7837dbec08d432f2075002612b7
neleon10 <cvn45@hotmail.com>
2024-06-16 12:24:21 +0200

    class replaced by className

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -12,8 +12,8 @@ function NavBar() {
   };
 
   return (
-    <header class=" bg-white fixed z-10 w-full shadow-lg box-shadow:1px 2px 15px rgba(100,100,100,0.3)">
-      <nav class="flex flex-row-reverse relative h-32 justify-between items-center px-7 py-4  sm:flex-row md:max-w-5xl md:m-auto  ">
+    <header className=" bg-white fixed z-10 w-full shadow-lg box-shadow:1px 2px 15px rgba(100,100,100,0.3)">
+      <nav className="flex flex-row-reverse relative h-32 justify-between items-center px-7 py-4  sm:flex-row md:max-w-5xl md:m-auto  ">
         <Image
           src={logo}
           alt="Bajda Logo"
@@ -26,19 +26,19 @@ function NavBar() {
           }  bg-white border-b-2 w-40 border-x-2 border-custom-green rounded flex flex-col absolute z-20 top-[126px] left-0 sm:w-auto sm:flex-row sm:static sm:border-t-2 sm:border-none  sm:opacity-100 md:mr-16 md:text-lg font-semibold	
             `}
         >
-          <li class="py-0.5 pl-9 sm:pl-0 text-[1.12rem] mr-2 md:mr-6 xl:mr-10">
+          <li className="py-0.5 pl-9 sm:pl-0 text-[1.12rem] mr-2 md:mr-6 xl:mr-10">
             <Link href={"#home"} className="relative group">
               Home
               <span className="absolute inset-x-0 bottom-0 h-0.5 bg-green-500 transform origin-right transition-all duration-500 scale-x-0 group-hover:scale-x-100 group-hover:origin-left"></span>
             </Link>
           </li>
-          <li class="py-0.5 pl-9 sm:pl-0 text-[1.12rem] mr-2 md:mr-6 xl:mr-10">
+          <li className="py-0.5 pl-9 sm:pl-0 text-[1.12rem] mr-2 md:mr-6 xl:mr-10">
             <Link href={"#nosotros"} className="relative group">
               Nosotros
               <span className="absolute inset-x-0 bottom-0 h-0.5 bg-green-500 transform origin-right transition-all duration-500 scale-x-0 group-hover:scale-x-100 group-hover:origin-left"></span>
             </Link>
           </li>
-          <li class="py-0.5 pl-9 sm:pl-0 text-[1.12rem] mr-2 md:mr-6 xl:mr-10">
+          <li className="py-0.5 pl-9 sm:pl-0 text-[1.12rem] mr-2 md:mr-6 xl:mr-10">
             <Link href={"#contacto"} className="relative group">
               Contacto
               <span className="absolute inset-x-0 bottom-0 h-0.5 bg-green-500 transform origin-right transition-all duration-500 scale-x-0 group-hover:scale-x-100 group-hover:origin-left"></span>
