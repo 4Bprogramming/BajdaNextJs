7b1ec9b06f2a35f87c656b9b25e092c31ff18909
neleon10 <cvn45@hotmail.com>
2024-06-20 00:31:20 +0200

    next auth options added

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -6,7 +6,15 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
-  ]
+  ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      if (account.provider === "google") {
+        return profile.email_verified && profile.email.match("carlosvazqueznosetto@gmail.com");
+      }
+      return true 
+    },
+  }
 });
 
 export { handler as GET, handler as POST };
