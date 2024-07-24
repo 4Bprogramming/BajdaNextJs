691871e01cfeea296dbc2339144e6e9982308219
4Bprogramming <4b.programming@gmail.com>
2024-07-24 17:45:03 -0400

    Update ContactMedia.jsx

--- src/Components/Contact/ContactMedia.jsx
+++ src/Components/Contact/ContactMedia.jsx
@@ -8,7 +8,7 @@ function ContactMedia() {
       <contact-us class="flex flex-col items-start ml-4 w-full ">
         <h4 class=" text-2xl font-semibold text-grey-1">Contáctanos</h4>
         <p>Correo: <Link href={"mailto:info@estudiomartinbajda.com.ar"}>estudiomartinbajda@gmail.com</Link></p>
-        <p>Teléfono: <span class="text-grey-1">+5492944581548</span></p>
+        <p>Teléfono: <span class="text-grey-1">+54 9 294 458 1548</span></p>
         <Link className='relative underline underline-offset-2 hover:text-custom-green transition-all duration-700 group hover:ml-2' href={"https://www.instagram.com/arq.martin_bajda/?igsh=YmhqdHNqdmxlZGJs"} target='blank'>
         Visitá Nuestro Instagram: 
         <FaInstagram className="text-green-500 h-6 w-6 cursor-pointer absolute top-0 -right-8 group-hover:text-black   transition-all duration-700" />
