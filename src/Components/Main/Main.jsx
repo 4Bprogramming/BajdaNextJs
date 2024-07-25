35769767a286b83ab8fdba3fb88ff765c2056568
Carlos Vazquez Nosetto <78918123+neleon10@users.noreply.github.com>
2024-07-25 21:23:51 +0200

    Update Main.jsx

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -27,7 +27,7 @@ function Main() {
           <article className="absolute top-6 left-2 sm:top-9 md:top-20 lg:left-6 p-4 rounded-lg" style={{ backgroundColor: 'rgba(44, 38, 36 , 0.6)' }}>
             <p className="text-white md:text-4xl lg:text-5xl">Arquitecto</p>
             <h1 className="text-white max-320:text-3xl text-4xl md:text-7xl lg:text-8xl ">
-              <span className="text-custom-green max-320:text-3xl text-5xl md:text-7xl lg:text-9xl">
+              <span className="text-custom-green">
                 Arq.
               </span>{" "}
               Martín Bajda
